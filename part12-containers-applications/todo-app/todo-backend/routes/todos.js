1bd8092640ebfe982d603de1e92caa336be6153c
Riku Olkkonen <riku.olkkonen@aalto.fi>
2022-07-16 11:52:01 +0300

    exercise 12.7 done

--- part12-containers-applications/todo-app/todo-backend/routes/todos.js
+++ part12-containers-applications/todo-app/todo-backend/routes/todos.js
@@ -35,12 +35,14 @@ singleRouter.delete('/', async (req, res) => {
 
 /* GET todo. */
 singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  res.send(req.todo);
 });
 
 /* PUT todo. */
 singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  console.log(req);
+  const updatedTodo = await Todo.updateOne(req.todo, req.body);
+  res.send(updatedTodo);
 });
 
 router.use('/:id', findByIdMiddleware, singleRouter)
